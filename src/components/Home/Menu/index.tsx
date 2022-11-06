736e73dec30be90a5179cc8a2efb8316579c2ffd
empal030110 <73802396+empal030110@users.noreply.github.com>
2022-11-06 17:40:05 +0900

    Link -> a

--- src/components/Home/Menu/index.tsx
+++ src/components/Home/Menu/index.tsx
@@ -1,20 +1,22 @@
+import { Link } from 'react-router-dom';
+
 const Menu = () => {
   return (
-    <div className='flex justify-between max-w-page-full mx-auto'>
+    <div className="flex justify-between max-w-page-full mx-auto">
       <div>
-        <a href='/'>홈</a>
-        <a href='/my' className='ml-4'>
+        <a href="#">홈</a>
+        <a href="#" className="ml-4">
           my
         </a>
-        <a href='/community' className='ml-4'>
+        <a href="#" className="ml-4">
           커뮤니티
         </a>
       </div>
       <div>
-        <a href='/login' className='ml-4'>
+        <a href="#" className="ml-4">
           로그인
         </a>
-        <a href='/signup' className='ml-4'>
+        <a href="#" className="ml-4">
           회원가입
         </a>
       </div>
