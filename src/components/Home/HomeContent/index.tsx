09bffe7c541510eacfd9ef0fd63cf3dfea47844c
empal030110 <73802396+empal030110@users.noreply.github.com>
2022-11-13 16:12:20 +0900

    HomeList merge cancle

--- src/components/Home/HomeContent/index.tsx
+++ src/components/Home/HomeContent/index.tsx
@@ -1,5 +1,16 @@
+import HomeList from './HomeList';
+
 const HomeContent = () => {
-  return <div className='bg-white mt-16 h-80'>dd</div>;
+  return (
+    <div className="w-full flex mt-8 h-[364px]">
+      <div className="w-full h-full bg-white">
+        <HomeList />
+      </div>
+      {/* <div className='w-[50%] h-full bg-white'>
+        <HomeList />
+      </div> */}
+    </div>
+  );
 };
 
 export default HomeContent;
