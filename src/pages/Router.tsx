5f503a95ca51ead106ab3fc3e2267f0ed49c407e
Kim jonghun <developerkimjh@gmail.com>
2023-01-06 00:09:05 +0900

    FEAT: UserInfo 레이아웃

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Login from 'pages/login';
 import Home from 'pages/home';
-import Signup from 'pages/Signup';
 import Search from 'pages/search';
 
 import Menu from 'components/Menu';
@@ -15,9 +13,7 @@ const Router = () => {
       <Routes>
         <Route>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/login" element={<Login />}></Route>
           <Route path="/search" element={<Search />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
         </Route>
       </Routes>
       <ModalManager />
--- src/pages/login/index.tsx
+++
@@ -1,5 +0,0 @@
-const Login = () => {
-  return <div>dd</div>;
-};
-
-export default Login;
