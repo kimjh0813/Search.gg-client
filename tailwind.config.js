eb194d1a0f6a2de098048a15a22cc53d8fa58821
Kim jonghun <developerkimjh@gmail.com>
2023-01-12 23:01:58 +0900

    FEAT: tailwind setting 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
         'page-full': '1080px ',
       },
     },
+    screens: {
+      xsm: '570px',
+      sm: '640px',
+      md: '768px',
+      mlg: '900px',
+      lg: '1024px',
+      xl: '1280px',
+    },
   },
   plugins: [],
 };
