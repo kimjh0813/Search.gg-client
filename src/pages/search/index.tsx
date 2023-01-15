1ebca5455e25e8f2c3490fdd32846fb4d9162c17
Kim jonghun <developerkimjh@gmail.com>
2023-01-15 14:15:15 +0900

    FEAT: UserGameRecord Component 추가

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -2,34 +2,21 @@ import { getUserGameRecord, getUserInfo } from 'api/search';
 import { useEffect, useState } from 'react';
 import UserProfile from './UserProfile';
 
-import type * as T from './type';
+import type { UserInfo } from 'types/search/UserTier';
+import UserGameRecord from './UserGameRecord';
 
 const Search = () => {
-  const [userInfo, setUserInfo] = useState<T.UserInfo>();
+  const [userInfo, setUserInfo] = useState<UserInfo>();
 
   useEffect(() => {
     const userName = window.location.pathname.replaceAll('/search/', '');
     getUserInfo(userName).then((result) => setUserInfo(result));
   }, []);
 
-  useEffect(() => {
-    getGameRecord();
-  }, [userInfo?.puuid]);
-
-  const getGameRecord = async () => {
-    if (!userInfo?.puuid) return;
-
-    const result = await getUserGameRecord({
-      puuid: userInfo?.puuid,
-      type: 'normal',
-      count: 40,
-    });
-    console.log(result);
-  };
-
   return (
     <div className="max-w-page-full m-auto px-4">
       <UserProfile userInfo={userInfo} />
+      <UserGameRecord userInfo={userInfo} />
     </div>
   );
 };
