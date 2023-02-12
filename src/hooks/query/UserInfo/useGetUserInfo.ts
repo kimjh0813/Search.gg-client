e7bb06d5e7125cd21f6ec31f39ca6df5c71e9b4a
Kim jonghun <developerkimjh@gmail.com>
2023-02-12 19:06:30 +0900

    FEAT: useGetUserInfo react-query 추가

---
+++ src/hooks/query/UserInfo/useGetUserInfo.ts
@@ -0,0 +1,25 @@
+import { useCallback } from 'react';
+
+import { getUserInfo } from 'api/search';
+import { useQuery } from 'react-query';
+import queryClient from 'utils/QueryClient';
+
+interface UserInfoProps {
+  userName: string;
+}
+
+const useGetUserInfo = ({ userName }: UserInfoProps) => {
+  const response = useQuery(['USER_INFO'], async () => {
+    const response = await getUserInfo(userName);
+
+    return response;
+  });
+
+  const invalidate = useCallback(() => {
+    queryClient.invalidateQueries(['USER_INFO']);
+  }, []);
+
+  return [{ ...response }, invalidate] as const;
+};
+
+export default useGetUserInfo;
