ee7c915b558a1206c62b86f4cfdd082bdd2b0c55
Kim jonghun <developerkimjh@gmail.com>
2023-02-13 21:01:19 +0900

    FIX: useGetTierInfo 적용

--- src/pages/search/UserProfile/index.tsx
+++ src/pages/search/UserProfile/index.tsx
@@ -4,50 +4,40 @@ import { Button } from 'antd';
 import { getGameVersion, getUserTier } from 'api/search';
 import UserTier from 'components/TierInfo';
 import { useGetVersion } from 'hooks/query';
+import useGetTierInfo from 'hooks/query/TierInfo/useGetTierInfo';
 import type { TierInfo, UserInfo } from 'types/search';
 
 import * as S from './styled';
 
 const UserProfile = ({ userInfo }: { userInfo: UserInfo }) => {
   const [{ data: version }] = useGetVersion();
-
-  const [tierInfo, setTierInfo] = useState<TierInfo[]>();
-
-  useEffect(() => {
-    getTier();
-  }, [userInfo]);
-
-  const getTier = async () => {
-    if (!userInfo.id) return;
-    const response = await getUserTier(userInfo.id);
-
-    if (!response) return;
-    setTierInfo(response);
-  };
+  const [{ data: tierInfo }] = useGetTierInfo({ userId: userInfo.id });
 
   return (
     <S.Container className='flex-col mlg:flex-row mlg:justify-between '>
       <S.Box className='px-4 py-6 xsm:px-8'>
         <S.ProfileBox>
-          {version && userInfo && (
+          {version && (
             <>
               <img
-                src={`http://ddragon.leagueoflegends.com/cdn/${version}/img/profileicon/${userInfo?.profileIconId}.png`}
+                src={`http://ddragon.leagueoflegends.com/cdn/${version}/img/profileicon/${userInfo.profileIconId}.png`}
               />
-              <span>{userInfo?.summonerLevel}</span>
+              <span>{userInfo.summonerLevel}</span>
             </>
           )}
         </S.ProfileBox>
         <S.InfoBox>
-          <S.NameBox>{userInfo?.name}</S.NameBox>
+          <S.NameBox>{userInfo.name}</S.NameBox>
           <Button type='primary' className='mt-2 ml-6 border rounded w-[100px]'>
             전적 갱신
           </Button>
         </S.InfoBox>
       </S.Box>
-      <S.Box className='px-4 py-6 mgl:px-6'>
-        <UserTier tierInfo={tierInfo} />
-      </S.Box>
+      {tierInfo && (
+        <S.Box className='px-4 py-6 mgl:px-6'>
+          <UserTier tierInfo={tierInfo} />
+        </S.Box>
+      )}
     </S.Container>
   );
 };
